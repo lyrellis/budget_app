b04509ad6b96b36047cf738c2a74e98fe0d98616
Lyrellis <tmjenkins347@proton.me>
2025-09-12 23:43:13 -0400

    even more progress 9/12/25

--- budget_app.py
+++ budget_app.py
@@ -1,7 +1,3 @@
-#class Ledger:
-#    def __init__(self, category)
-#        self.ledger = category
-
 class Category:
     def __init__(self, category):
         self.category = category
@@ -11,34 +7,44 @@ class Category:
     def __str__(self):
         category_str = f'{self.category:*^30}'
         for transaction in self.ledger:
-            amount, description = transaction
-            amount_as_currency = f'{float(amount):.2f}'
-            category_str += f'\n{description[:23]:<23}{amount_as_currency:>7}'
+            amount_as_currency = f'{float(transaction['amount']):.2f}'
+            category_str += f'\n{transaction['description'][:23]:<23}{amount_as_currency:>7}'
         category_str += f'\nTotal: {self.total:.2f}'
         return category_str
 
     def deposit(self, amount, description = ''):
-        self.ledger.append((amount, description))
+        self.ledger.append({'amount': amount, 'description': description})
         self.total += amount
 
-    def withdraw(self, amount, description = ''):
-        self.ledger.append((f'-{amount}', description))
-        self.total -= amount
+    def check_funds(self, amount):
+        if amount > self.total:
+            return False
+        else:
+            return True
 
-    def get_balance(self):
-        pass
+    def withdraw(self, amount, description = ''):
+        if self.check_funds(amount):
+            self.ledger.append({'amount': -amount, 'description': description})
+            self.total -= amount
+            return True
+        else:
+            return False
 
     def transfer(self, amount, category):
-        self.ledger.append((f'-{amount}', f'Transfer to {category.category}'))
-        category.ledger.append((amount, f'Transfer from {self.category}'))
-        self.total -= amount
-        category.total += amount
+        if self.check_funds(amount):
+            self.ledger.append({'amount': -amount, 'description': f'Transfer to {category.category}'})
+            category.ledger.append({'amount': amount, 'description': f'Transfer from {self.category}'})
+            self.total -= amount
+            category.total += amount
+            return True
+        else:
+            return False
 
-    def check_funds(self):
-        pass
+    def get_balance(self):
+        return f'{self.total:.2f}'
 
-def create_spend_chart(categories):
-    pass
+def create_spend_chart(categories=None):
+    return 'Percentage spent by category'
 
 # Example Input------------------
 
@@ -50,3 +56,5 @@ clothing = Category('Clothing')
 food.transfer(50, clothing)
 print(food)
 print(clothing)
+
+print(create_spend_chart())
